cfca087f2d42039a78bf80e210bb4c72a0ca27f7
yair halpern <its.yak@gmail.com>
2012-10-12 23:31:46 +0200

    added fallback lcfirst() and ucfirst() to support PHPversion<5.3

--- YkMVC_v02/website/lib/core.php
+++ YkMVC_v02/website/lib/core.php
@@ -6,6 +6,23 @@ if( !empty($_POST['_method']) && in_array($_POST['_method'], array('put', 'delet
 }
 */
 
+//fix :
+if (!function_exists('lcfirst')) {
+	function lcfirst($string) {
+		$string[0] = strtolower($string[0]);
+		return $string;
+	}
+}
+
+if (!function_exists('ucfirst')) {
+	function lcfirst($string) {
+		$string[0] = strtoupper($string[0]);
+		return $string;
+	}
+}
+
+
+
 include_once 'Object.php';
 include_once 'Controller.php';
 include_once 'Model.php';
